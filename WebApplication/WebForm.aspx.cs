81e66820782c40896b53a8627c2556de7837f366
ruemura <rafael.uemura@gmail.com>
2016-03-21 11:19:18 -0300

    resultSet_Click

--- WebApplication/WebForm.aspx.cs
+++ WebApplication/WebForm.aspx.cs
@@ -16,17 +16,16 @@ namespace WebApplication
 
         protected void TextBox_TextChanged(object sender, EventArgs e)
         {
+                        
+        }
 
+        protected void resultSet_Click(object sender, EventArgs e)
+        {
             string name = TextBoxName.Text;
             string quest = TextBoxQuest.Text;
             string colour = TextBoxFavColour.Text;
             string resultSet = "Right, " + name + ", off you go to get your " + colour + " " + quest;
             resultLabel.Text = resultSet;
-        }
-
-        protected void resultSet_Click(object sender, EventArgs e)
-        {
-            
         }
     }
 }
